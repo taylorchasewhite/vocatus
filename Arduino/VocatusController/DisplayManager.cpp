a535a5acd626bfc11f3c27ee318a7dd2a53ac9e0
Taylor White <taylor@taylorchasewhite.com>
2018-06-06 19:20:18 -0500

    Fix method definitions in Storage manager
    Remove dot and dash functions, display manager.

--- Arduino/VocatusController/DisplayManager.cpp
+++ Arduino/VocatusController/DisplayManager.cpp
@@ -16,18 +16,3 @@ StorageManager::StorageManager(int pin)
   _pin = pin;
 }
 
-void StorageManager::dot()
-{
-  digitalWrite(_pin, HIGH);
-  delay(250);
-  digitalWrite(_pin, LOW);
-  delay(250);  
-}
-
-void StorageManager::dash()
-{
-  digitalWrite(_pin, HIGH);
-  delay(1000);
-  digitalWrite(_pin, LOW);
-  delay(250);
-}
