fe269f7f438cf7f869ff6cc93090e3668ed0ba98
taylorchasewhite <taylor@taylorchasewhite.com>
2018-07-04 21:26:52 -0500

    Adding iPhone App

---
+++ Applications/iOS/Vocatus/Vocatus/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Vocatus
+//
+//  Created by Taylor White on 7/4/18.
+//  Copyright © 2018 Taylor White. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
