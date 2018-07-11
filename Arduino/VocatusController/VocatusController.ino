b11694e190fd06333024e4607d9f9fdd8a3b41c0
stephenlago <stephenlago@gmail.com>
2018-07-10 22:14:24 -0500

    Modifying Button functionality
    Changing from interrupts to digital reads. Also neccesitates removing the delay from Loop

--- Arduino/VocatusController/VocatusController.ino
+++ Arduino/VocatusController/VocatusController.ino
@@ -46,12 +46,12 @@ const int BITS_PER_SECOND = 9600; // initialize serial communication at 9600 bit
 
 const int PIN_FLOW = 2;
 const int PIN_RESET = 3;
-const int PIN_CYCLE_DISPLAY = 5;
+const int PIN_CYCLE_DISPLAY = 4;
+
+unsigned int MS_BETWEEN_DRINKS = 1000;
 
 // Counts
 volatile int flowCount; //@NOTE:: I don't think this needs to be volatile. It's only needed if the variable could be changed from outside of the code (e.g another class)
-int prevFlowCount;
-
 float multiplier;
 float mostRecentVolume;
 
@@ -66,6 +66,16 @@ int mostRecentDrinkTime;  // The time it took in ms to finish the last drink
 const unsigned long SECONDS_IN_DAY = 86400;
 unsigned long lastDrinkCompletionInstant; //@NOTE:: why are all of these globals?
 unsigned long currentDrinkCompletionInstant;
+
+const unsigned long DEBOUNCE_BUFFER = 50;
+unsigned long resetLastDebounce;
+unsigned long displayLastDebounce;
+
+//states
+int resetLastState;
+int resetCurState;
+int displayLastState;
+int displayCurState;
 
 // I/O Controls
 DisplayManager display;
@@ -84,15 +94,22 @@ void initGlobals() {
   multiplier = 3.05; //TCW: 2.647  Chode: 3.05
 
   flowCount = 0;
-  prevFlowCount = -1;
 
-  display = *new DisplayManager(DEBUG); //set it to whatever mode(s) you want: DEBUG|STATUSBOARD|LCD
+  display = *new DisplayManager(DEBUG|LCD); //set it to whatever mode(s) you want: DEBUG|STATUSBOARD|LCD
   storage = *new StorageManager();
   timeManager = *new TimeManager(10); // TODO Allow different initialization modes like DisplayManager
 
   //initialize all tracking variables to 0 in case they are not read from storage
   tonight = *new Record();
   lifetime = *new Record();
+
+  //start the button states at their expected starting values
+  resetLastState = HIGH;
+  displayLastState = HIGH;
+  resetCurState = HIGH;
+  displayCurState = HIGH;
+  resetLastDebounce = 0;
+  displayLastDebounce = 0;
 }
 
 /**
@@ -110,8 +127,6 @@ void setup() {
   pinMode(PIN_CYCLE_DISPLAY, INPUT_PULLUP);
   
   attachInterrupt(digitalPinToInterrupt(PIN_FLOW), Flow, RISING);
-  attachInterrupt(digitalPinToInterrupt(PIN_RESET), resetListener, RISING);
-  attachInterrupt(digitalPinToInterrupt(PIN_CYCLE_DISPLAY), displayButtonListener, RISING);
 
   printStatusReport();
 }
@@ -120,11 +135,19 @@ void setup() {
 /********************         Core          *********************/
 /****************************************************************/
 void loop() {
-  prevFlowCount=flowCount;
-  
-  interrupts();   //Enables interrupts on the Arduino
-  delay (1000);   //Wait 1 second 
-  noInterrupts(); //Disable the interrupts on the Arduino
+  int resetReading = digitalRead(PIN_RESET);
+  int displayReading = digitalRead(PIN_CYCLE_DISPLAY);
+
+  if(checkDebounce(resetReading,resetCurState,resetLastState,resetLastDebounce)) {
+    resetListener();
+  }
+  if(checkDebounce(displayReading,displayCurState,displayLastState,displayLastDebounce)) {
+    displayChangeListener();
+  }
+
+  resetLastState = resetReading;
+  displayLastState = displayReading;
+
   timeManager.manageTime();
   if(isDrinkOver()) {
     recordDrinkEnd();
@@ -148,28 +171,63 @@ void Flow() {
 /**
  * Called when the display cycle button is pressed.
  */
-void displayButtonListener() {
+void displayChangeListener() {
   Serial.println("====Executed LCDListener====");
-    display.DebugPrintln("  ==LCD Cycle Button pressed==");
-    display.CycleCurrentValueToDisplay();
-    printStatusReport();
+    //display.DebugPrintln("  ==LCD Cycle Button pressed==");
+    //display.CycleCurrentValueToDisplay();
+    //printStatusReport();
+}
+
+/**
+ * Called when the display cycle button is pressed.
+ */
+
+bool checkDebounce(int newReading, int buttonState, int lastState, unsigned long lastDebounce) {
+  bool returnValue = false;
+  
+  if(newReading != lastState) {
+    lastDebounce = millis(); //reset debouncing timer
+  }
+
+  //wait until the buffer period has passed
+  if((millis() - lastDebounce) > DEBOUNCE_BUFFER) {
+
+    //if the state has changed
+    if(newReading != buttonState) {
+      buttonState = newReading;
+
+      //only return true if the button is depressed (and not lifted up)
+      if(buttonState == LOW) {
+        returnValue = true;
+      }
+    }
+  }
+
+  return(returnValue);
 }
 
 /****************************************************************/
 /********************        Timing         *********************/
 /****************************************************************/
 /**
+ * Method for getting the current time, intended to allow easy swapping of time management as needed
+ */
+unsigned long getCurrentTime() {
+  return(millis());
+}
+
+/**
  * Called the first time the hall effect sensor has been triggered since the last drink session completed.
  */
 void startDrink() {
-  startTime=millis();
+  startTime=getCurrentTime();
 }
 
 /**
  * Called every time the hall effect sensor fires. Represents another ~2.65 ml have been drank.
  */
 void drinkPulse() {
-  endTime=millis();
+  endTime=getCurrentTime();
 }
 
 /**
@@ -237,7 +295,6 @@ void reset() {
 void resetCurrentDrink() {
   //reset volume variables
   flowCount=0;
-  prevFlowCount=-1;
   mostRecentVolume = 0.0;
 
   //reset timing variables
@@ -273,7 +330,7 @@ void resetLifetimeRecord() {
  * Second check makes sure that we don't cut off a drink mid-stream
  */
 boolean isDrinkOver() {
-  return ((flowCount>0) && (flowCount==prevFlowCount));
+  return ((flowCount>0) && (endTime + MS_BETWEEN_DRINKS < millis()));
 }
 
 /**
